ad640c5241e2f5039955ff72855d281fe7fd2541
Scott Milliorn <scottmilliorn@gmail.com>
2021-06-16 13:17:38 -0700

    convert to verbatim string. removed firstEncounter

--- ConsoleGame/Program.cs
+++ ConsoleGame/Program.cs
@@ -9,9 +9,9 @@ namespace ConsoleGame
             Console.WriteLine("ButtNekid's Bounce House");
             Console.Write("What's your name:");
 
-            var name = Console.ReadLine();
+            string name = Console.ReadLine();
 
-            Console.WriteLine("You awake in a cold stone, dark room. You feel dazed and are having trouble remembering anything about your past.");
+            Console.WriteLine($"You awake in a cold stone, dark room. You feel dazed and are having trouble remembering anything about your past.");
 
             if (String.IsNullOrWhiteSpace(name))
             {
@@ -22,7 +22,11 @@ namespace ConsoleGame
                 Console.WriteLine($"You know your name is {name}.");
             }
 
-            Console.WriteLine("You grope around in the darkness until you find a door handle. You feel some resistance as\nyou turn the handle, but the rusty lock breaks with little effort. You see your captor\nstanding with his back to you outside the door.");
+            Console.WriteLine($@"You grope around in the darkness until you find a door handle. You feel some resistance as
+you turn the handle, but the rusty lock breaks with little effort. You see your captor
+standing with his back to you outside the door.
+You throw open the door and grab a rusty metal sword while charging toward your captor
+He turns...");
         }
     }
 }
