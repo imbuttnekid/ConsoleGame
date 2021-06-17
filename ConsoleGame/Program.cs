a1637339e0427155de0d8b5caa6e0866776c2892
Scott Milliorn <scottmilliorn@gmail.com>
2021-06-16 21:27:37 -0700

    format

--- ConsoleGame/Program.cs
+++ ConsoleGame/Program.cs
@@ -42,6 +42,7 @@ you turn the handle, but the rusty lock breaks with little effort. You see your 
 standing with his back to you outside the door.
 You throw open the door and grab a rusty metal sword while charging toward your captor
 He turns...");
+
             Thread.Sleep(100);
             Enemy rogue = new Enemy("Human Rogue", 1, 1, 1);
 
