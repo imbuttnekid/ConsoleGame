63850451bb79850b4f57bb48f1f4525a9b3c4319
Scott Milliorn <scottmilliorn@gmail.com>
2021-06-16 21:26:36 -0700

    Threading

--- ConsoleGame/Program.cs
+++ ConsoleGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace ConsoleGame
 {
@@ -7,6 +8,7 @@ namespace ConsoleGame
         static void Main(string[] args)
         {
             Console.WriteLine("ButtNekid's Bounce House");
+            Thread.Sleep(1000);
             Console.Write("What's your name:");
 
             string name = Console.ReadLine();
@@ -16,11 +18,14 @@ namespace ConsoleGame
             byte ap = (byte)random.Next(8, 20);
 
             Player player = new Player(name, hp, ac, ap);
-            System.Console.WriteLine($"Starting Armor class:{player.ArmorClass}");
-            System.Console.WriteLine($"Starting Attack Points:{player.AttackPoints}");
-            System.Console.WriteLine($"Starting Hitpoints:{player.HitPoints}");
-
+            Console.WriteLine($"Starting Armor class:{player.ArmorClass}");
+            Thread.Sleep(100);
+            Console.WriteLine($"Starting Attack Points:{player.AttackPoints}");
+            Thread.Sleep(100);
+            Console.WriteLine($"Starting Hitpoints:{player.HitPoints}");
+            Thread.Sleep(100);
             Console.WriteLine($"You awake in a cold stone, dark room. You feel dazed and are having trouble remembering anything about your past.");
+            Thread.Sleep(100);
 
             if (String.IsNullOrWhiteSpace(name))
             {
@@ -31,13 +36,18 @@ namespace ConsoleGame
                 Console.WriteLine($"You know your name is {name}.");
             }
 
+            Thread.Sleep(1000);
             Console.WriteLine($@"You grope around in the darkness until you find a door handle. You feel some resistance as
 you turn the handle, but the rusty lock breaks with little effort. You see your captor
 standing with his back to you outside the door.
 You throw open the door and grab a rusty metal sword while charging toward your captor
 He turns...");
-
+            Thread.Sleep(100);
             Enemy rogue = new Enemy("Human Rogue", 1, 1, 1);
+
+            Console.WriteLine($"You encounter a {rogue.Name}");
+            Thread.Sleep(100);
+            Console.WriteLine("| (A)ttack (D)efend |\n| (R)un    (H)eal   |");
         }
     }
 }
